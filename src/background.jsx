132077ead72e46287a756f93dd39c9126791d3a0
Gabriela Asuncion <gabrielaasuncion@gmail.com>
2018-12-13 02:13:43 -0500

    lit

--- src/background.jsx
+++ src/background.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import bg from './painted-ladies-3.jpg';
+import aqua from './aqua.ttf'
 import './App.css';
 
 class Background extends Component {
@@ -7,6 +8,16 @@ class Background extends Component {
     return (
       <div className="background-cont">
           <img className="bg" src={bg} alt="painted"/>
+          <a href=""><p id="title">Gabriela
+          <br />
+          Asuncion</p></a>
+          <ul id="menu">
+            <a href=""><li>About</li></a>
+            <br/>
+            <a href=""><li>Resume</li></a>
+            <br/>
+            <a href=""><li>Projects</li></a>
+          </ul>
       </div>
     );
   }
